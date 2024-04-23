9cba9e291772c5ce8fce64fa3bc089d74c3d1d43
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-04-23 18:17:31 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume of sphere
+ */
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function checkNumber() {
+  // input
+
+  const userNumber = parseFloat(document.getElementById("user-number").value)
+
+  // process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("result").innerHTML =
+      "You have guessed the correct number!"
+  }
+
+  if (userNumber != randomNumber) {
+    //output
+    document.getElementById("result").innerHTML =
+      "You have guessed the wrong number!"
+  }
+}
